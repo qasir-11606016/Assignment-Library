7189ad22d7775365cdd05b8a038be427b43be273
qasir-11606016 <aliqasier@hotmail.com>
2016-09-27 19:16:24 +1000

    create test for book class
    In this we create four test case which sucssesful pass and constructor
    and get methods

---
+++ src/testCase/testBookclass.java
@@ -0,0 +1,41 @@
+package testCase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+import library.entities.Book;
+import library.interfaces.entities.IBook;
+
+public class testBookclass {
+
+	
+
+	@Test
+	public void testgetauthor() {
+		IBook book1=new Book("Book1","title1","24",23);
+		assertEquals("Book1",book1.getAuthor());
+		
+	}
+	@Test
+	public void testGetTitle() {
+		IBook book1=new Book("Book1","title1","24",23);
+		assertEquals("title1",book1.getTitle());
+		
+	}
+	@Test
+	public void testGetCallNumber() {
+		IBook book1=new Book("Book1","title1","24",23);
+		assertEquals("24",book1.getCallNumber());
+	}
+	@Test
+	public void testGetId(){
+		IBook book1=new Book("Book1","title1","24",23);
+		assertEquals(23,book1.getID());
+	}
+	
+	
+
+}
