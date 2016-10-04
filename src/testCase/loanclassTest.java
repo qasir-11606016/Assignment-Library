4fb4add064238a59da347c734764fe7bf33d5600
qasir-11606016 <aliqasier@hotmail.com>
2016-10-04 14:54:24 +1100

    test  for Barrower id

--- src/testCase/loanclassTest.java
+++ src/testCase/loanclassTest.java
@@ -18,7 +18,7 @@ import library.interfaces.entities.IMember;
 public class loanclassTest {
 
 	@Test 
-	public void classConstructortest() throws ParseException {
+	public void bookTest() throws ParseException {
 		IBook book1=new Book("jerry","LongLife","2411",23);
 		IMember member1=new Member("Alex","smith","045055455","smith@gmail.com",45);
 		String date1="08/09/2016";
@@ -39,8 +39,30 @@ public class loanclassTest {
 		assertEquals(book1,loan.getBook());
 		
 		
-		
+		assertEquals(member1,loan.getBorrower());
 
+	}
+	@Test 
+	public void memberTest() throws ParseException {
+		IBook book1=new Book("jerry","LongLife","2411",23);
+		IMember member1=new Member("Alex","smith","045055455","smith@gmail.com",45);
+		String date1="08/09/2016";
+		String date2="10/09/2016";
+		
+		SimpleDateFormat df = new SimpleDateFormat("dd/mm/yyyy");
+		Date testDate = null;
+		try {
+			testDate = (Date) df.parse(date1);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Date testDate1=(Date) df.parse(date2);
+		
+		ILoan loan=new Loan(book1,member1,testDate,testDate1);
+		
+		assertEquals(member1,loan.getBorrower());
+		
 	}
 
 }
