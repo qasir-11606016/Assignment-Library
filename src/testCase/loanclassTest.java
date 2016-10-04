1a1e198399cd61b47ec021a6b3c18c427168014f
qasir-11606016 <aliqasier@hotmail.com>
2016-10-04 14:49:39 +1100

    TestCase for loan class

--- src/testCase/loanclassTest.java
+++ src/testCase/loanclassTest.java
@@ -2,17 +2,44 @@ package testCase;
 
 import static org.junit.Assert.*;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import org.junit.Test;
 
 import library.entities.Book;
+import library.entities.Loan;
+import library.entities.Member;
 import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
 
 public class loanclassTest {
 
 	@Test 
-	public void classConstructortest() {
+	public void classConstructortest() throws ParseException {
 		IBook book1=new Book("jerry","LongLife","2411",23);
-		ILoan 
+		IMember member1=new Member("Alex","smith","045055455","smith@gmail.com",45);
+		String date1="08/09/2016";
+		String date2="10/09/2016";
+		
+		SimpleDateFormat df = new SimpleDateFormat("dd/mm/yyyy");
+		Date testDate = null;
+		try {
+			testDate = (Date) df.parse(date1);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Date testDate1=(Date) df.parse(date2);
+		
+		ILoan loan=new Loan(book1,member1,testDate,testDate1);
+		
+		assertEquals(book1,loan.getBook());
+		
+		
+		
 
 	}
 
