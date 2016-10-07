5f8ca57d3e79bfe5efefaa3123bd279ebbcd607d
qasir-11606016 <aliqasier@hotmail.com>
2016-10-07 21:21:21 +1100

    Ibook helper object is created for the test

--- src/testCase/testBookMapDao.java
+++ src/testCase/testBookMapDao.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import library.daos.BookHelper;
 import library.daos.BookMapDAO;
+import library.interfaces.daos.IBookHelper;
 
 public class testBookMapDao {
 
 	@Test
 	public void test() {
+		IBookHelper bookHelperObject =new BookHelper();
 		BookMapDAO object1=new BookMapDAO(null);
 
 
