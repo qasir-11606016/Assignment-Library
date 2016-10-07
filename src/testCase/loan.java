67b480aabef2440d6257f2a1d535bc076f9f063f
qasir-11606016 <aliqasier@hotmail.com>
2016-10-08 10:56:06 +1100

    loan objects is created

--- src/testCase/loan.java
+++ src/testCase/loan.java
@@ -2,6 +2,8 @@ package testCase;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 import library.entities.Book;
@@ -13,10 +15,14 @@ import library.interfaces.entities.IMember;
 public class loan {
 	IMember member1=new Member("Alex","smith","045055455","smith@gmail.com",45);
 	IBook book1=new Book("Book1","title1","24",23);
+	Date a =new Date();
+	
+	
 	@Test
 	public void loanconstructtest() {
-		Loan aaa=new Loan(book1,member1,null,null);
-		
+		Loan aaa=new Loan(book1,member1,a,a);
+		Loan bbb=new Loan(book1,member1,a,a);
+		assertEquals(aaa,bbb);
 	}
 
 }
