1571fe7dd2d209b8c15396921646282e747805e8
qasir-11606016 <aliqasier@hotmail.com>
2016-10-08 10:13:08 +1100

    loan class test function

---
+++ src/testCase/loan.java
@@ -0,0 +1,20 @@
+package testCase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import library.entities.Book;
+import library.entities.Member;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.IMember;
+
+public class loan {
+	IMember member1=new Member("Alex","smith","045055455","smith@gmail.com",45);
+	IBook book1=new Book("Book1","title1","24",23);
+	@Test
+	public void () {
+		fail("Not yet implemented");
+	}
+
+}
