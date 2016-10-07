71a8425a3c0f98bb120dbc8ba7957e8f40e8098f
qasir-11606016 <aliqasier@hotmail.com>
2016-10-08 10:25:37 +1100

    new test case  implement

--- src/testCase/loan.java
+++ src/testCase/loan.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import library.entities.Book;
+import library.entities.Loan;
 import library.entities.Member;
 import library.interfaces.entities.IBook;
 import library.interfaces.entities.IMember;
@@ -13,8 +14,9 @@ public class loan {
 	IMember member1=new Member("Alex","smith","045055455","smith@gmail.com",45);
 	IBook book1=new Book("Book1","title1","24",23);
 	@Test
-	public void () {
-		fail("Not yet implemented");
+	public void loanconstructtest() {
+		Loan aaa=new Loan(book1,member1,null,null);
+		
 	}
 
 }
