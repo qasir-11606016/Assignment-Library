228c220964b4b531d1d3f6070d6a099d4407cba0
qasir-11606016 <aliqasier@hotmail.com>
2016-10-07 21:25:51 +1100

    BookHelper Function Make book is used

--- src/testCase/testBookMapDao.java
+++ src/testCase/testBookMapDao.java
@@ -13,6 +13,7 @@ public class testBookMapDao {
 	@Test
 	public void test() {
 		IBookHelper bookHelperObject =new BookHelper();
+		bookHelperObject.makeBook("anderson", "BookTitle","24" , 64);
 		BookMapDAO object1=new BookMapDAO(null);
 
 
