d4421f2915df370d73a45b5ae56c95e74dfeb085
qasir-11606016 <aliqasier@hotmail.com>
2016-10-07 21:32:20 +1100

    using helper Class object in test

--- src/testCase/testBookMapDao.java
+++ src/testCase/testBookMapDao.java
@@ -14,7 +14,7 @@ public class testBookMapDao {
 	public void test() {
 		IBookHelper bookHelperObject =new BookHelper();
 		bookHelperObject.makeBook("anderson", "BookTitle","24" , 64);
-		BookMapDAO object1=new BookMapDAO(null);
+		BookMapDAO object1=new BookMapDAO(bookHelperObject);
 
 
 	}
