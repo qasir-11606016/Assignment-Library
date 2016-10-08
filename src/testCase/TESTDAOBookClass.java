22835e0a3941b757f3902609724e8174fc1d8659
qasir-11606016 <aliqasier@hotmail.com>
2016-10-08 11:17:29 +1100

    new class for test Class DAO

---
+++ src/testCase/TESTDAOBookClass.java
@@ -0,0 +1,14 @@
+package testCase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TESTDAOBookClass {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
