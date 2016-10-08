dce1b5fc22947ff79f15f7a1b36bb4faeb53ce48
qasir-11606016 <aliqasier@hotmail.com>
2016-10-08 11:22:54 +1100

    new object is created for book helper to test constructor

--- src/testCase/TESTDAOBookClass.java
+++ src/testCase/TESTDAOBookClass.java
@@ -6,9 +6,12 @@ import java.util.Date;
 
 import org.junit.Test;
 
+import library.daos.BookHelper;
+import library.daos.BookMapDAO;
 import library.entities.Book;
 import library.entities.Loan;
 import library.entities.Member;
+import library.interfaces.daos.IBookHelper;
 import library.interfaces.entities.IBook;
 import library.interfaces.entities.IMember;
 
@@ -16,13 +19,20 @@ public class TESTDAOBookClass {
 	
 	IMember member1=new Member("Alex","smith","045055455","smith@gmail.com",45);
 	IBook book1=new Book("Book1","taitle1","24",23);
-	IBook book1=new Book("Book1","taitle1","24",24);
+	IBook book2=new Book("Book1","taitle1","24",24);
+	IBook book3=new Book("Book1","taitle1","24",25);
+	IBookHelper bookHelperObject =new BookHelper();
+	
+	
+	BookMapDAO object1=new BookMapDAO(bookHelperObject);
+	
 	
 	Date a =new Date();
 	Loan aaa=new Loan(book1,member1,a,a);
 
 	@Test
 	public void test() {
+		IBook book15=bookHelperObject.makeBook("anderson", "BookTitle","24" , 64);
 		
 	}
 
