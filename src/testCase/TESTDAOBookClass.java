618f27386635f784973286b77fe241b2626e82fd
qasir-11606016 <aliqasier@hotmail.com>
2016-10-08 15:45:46 +1100

    test For finding book

--- src/testCase/TESTDAOBookClass.java
+++ src/testCase/TESTDAOBookClass.java
@@ -68,5 +68,11 @@ public class TESTDAOBookClass {
 		assertEquals(as,asa);
 		
 	}
+	public void test6() {
+		IBook as =object1.getBookByID(23);
+		IBook asa =object2.getBookByID(23);
+		assertEquals(as,asa);
+		
+	}
 
 }
