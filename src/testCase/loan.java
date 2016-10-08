cbecbc8ec743a4fc18fd5466dbdc5e7dd02e895c
qasir-11606016 <aliqasier@hotmail.com>
2016-10-08 11:00:29 +1100

    Test check over due functions

--- src/testCase/loan.java
+++ src/testCase/loan.java
@@ -14,19 +14,22 @@ import library.interfaces.entities.IMember;
 
 public class loan {
 	IMember member1=new Member("Alex","smith","045055455","smith@gmail.com",45);
-	IBook book1=new Book("Book1","title1","24",23);
+	IBook book1=new Book("Book1","taitle1","24",23);
 	Date a =new Date();
-	Loan aaa=new Loan(book1,member1,,a);
+	Loan aaa=new Loan(book1,member1,a,a);
 	Loan bbb=new Loan(book1,member1,a,a);
 	
 	
 	@Test
 	public void loanconstructtest() {
-		Loan aaa=new Loan(book1,member1,,a);
+		Loan aaa=new Loan(book1,member1,a,a);
 		Loan bbb=new Loan(book1,member1,a,a);
 		assertEquals(aaa,bbb);
 	}
 	@Test
-	public void loan() {}
+	public void loanCheckoverdate() {
+		assertEquals(aaa.checkOverDue(a),bbb.checkOverDue(a));
+		
+	}
 
 }
